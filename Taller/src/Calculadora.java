0f5a7709bc2e0b8943d0f2972bc5c024381fdf89
Angel Fernandez <angel.fernandez@despegar.com>
2018-03-13 09:43:11 -0300

    13/3

--- Taller/src/Calculadora.java
+++ Taller/src/Calculadora.java
@@ -2,37 +2,60 @@ import java.util.Scanner;
 
 public class Calculadora {
 
-public static void main(String[] args) {
-	Scanner leer = new Scanner(System.in);
-	String opcion;
-	int numero1;
-	int numero2;
-	
-	System.out.println("Elije la operacion que desea realizar");
-	System.out.println("+");
-	System.out.println("-");
-	System.out.println("*");
-	System.out.println("/");
-	
-	opcion = leer.next();
-	System.out.println("Escribe el primer numero");
-	numero1 = leer.nextInt();
-	
-	System.out.println("Escribe el segundo numero");
-	numero2 = leer.nextInt();
-	
-	System.out.println("La opeacion a realizar es: " + numero1 + opcion + numero2);
-	System.out.println("Resultado de suma: " + (numero1 + numero2));
-	System.out.println("Resultado de resta: " + (numero1 - numero2));
-	System.out.println("Resultado de multiplicacion: " + (numero1 * numero2));
-	System.out.println("Resultado de division: " + (numero1 / numero2));
-	
-	
-	
-	
-	
-	
-	
-}
+	public static void main(String[] args) {
+		Scanner leer = new Scanner(System.in);
+		String opcion;
+		int numero1;
+		int numero2;
+		int resultado;
+
+		System.out.println("Elije la operacion que desea realizar:");
+		System.out.println("+" + " " + "-" + " " + "*" + " " + "/");
+
+		opcion = leer.next();
+		System.out.println("Escribe el primer numero");
+		numero1 = leer.nextInt();
+
+		System.out.println("Escribe el segundo numero");
+		numero2 = leer.nextInt();
+
+		// opcion = "0";
+		if (opcion.equals("+")) {
+			resultado = (numero1 + numero2);
+			System.out.println("La opeacion a realizar es: " + numero1 + opcion + numero2);
+			System.out.println("Resultado de la suma: " + (resultado));
+			resultado = leer.nextInt();
+		} 
+		  else if (opcion.equals("*")) {
+			resultado = (numero1 * numero2);
+			System.out.println("La opeacion a realizar es: " + numero1 + opcion + numero2);
+			System.out.println("Resultado de la multiplicacion: " + (resultado));
+			resultado = leer.nextInt();
+		}
+		  else if (opcion.equals("/")) {
+			if (numero2 == 0) {
+				System.out.println("La opeacion no es valida Numero 2 no puede ser 0 en una division");
+			} else {
+				resultado = (numero1 / numero2);
+				System.out.println("La opeacion a realizar es: " + numero1 + opcion + numero2);
+				System.out.println("Resultado de la division: " + (resultado));
+				resultado = leer.nextInt();
+			}
+
+		}
+		  else if (opcion.equals("-")) {
+				resultado = (numero1 * numero2);
+				System.out.println("La opeacion a realizar es: " + numero1 + opcion + numero2);
+				System.out.println("Resultado de la resta: " + (resultado));
+				resultado = leer.nextInt();
+			}
+		
+		// System.out.println("Resultado de suma: " + (numero1 + numero2));
+		// System.out.println("Resultado de resta: " + (numero1 - numero2));
+		// System.out.println("Resultado de multiplicacion: " + (numero1 *
+		// numero2));
+		// System.out.println("Resultado de division: " + (numero1 / numero2));
+
+	}
 
 }
